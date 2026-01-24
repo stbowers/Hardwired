92cc5e80db8fa8c63c234879139b80070c5e4459
stbowers <sean.t.bowers@gmail.com>
2026-01-23 21:21:50 -0700

    Add differentiation for transformer tiers

--- Assets/Scripts/Patches/TransformerPatches.cs
+++ Assets/Scripts/Patches/TransformerPatches.cs
@@ -37,28 +37,67 @@ namespace Hardwired.Patches
 
             var transformer = __instance.GetOrAddComponent<TransformerComponent>();
 
-            self.StepNormal = 50f;
-            self.StepSmall = 1f;
+            // Determine settings based on type of transformer
+            double nMin, nMax, step;
 
+            // Small transformer
+            if (self.PrefabName == "StructureTransformerSmall")
+            {
+                self.StepNormal = 50f;
+                self.StepSmall = 1f;
+                self.OutputMaximum = 500;
+
+                nMin = 0.5;
+                nMax = 2.0;
+                step = 0.01;
+            }
+            // Medium transformer
+            else if (self.OutputMaximum < 55_000)
+            {
+                self.StepNormal = 100f;
+                self.StepSmall = 10f;
+                self.OutputMaximum = 5_000;
+
+                nMin = 0.1;
+                nMax = 10.0;
+                step = 0.05;
+            }
+            // Large transformer
+            else
+            {
+                self.StepNormal = 1000f;
+                self.StepSmall = 100f;
+                self.OutputMaximum = 100_000;
+
+                nMin = 1;
+                nMax = 100.0;
+                step = 0.1;
+            }
+
+            // Calculate input & output voltages, and error between output voltage and desired setting
             var inputVoltage = transformer.PrimaryVoltage.Magnitude;
             var outputVoltage = transformer.SecondaryVoltage.Magnitude;
-
-            var minStep = 0.06 * inputVoltage;
-
             var dV = Math.Abs(self.Setting - outputVoltage);
 
+            // Minimum value of dV before we'll switch transformer ratio
+            var deadzone = 3 * step * inputVoltage;
+
             // If output voltage is out of range, change the ratio of the transformer
-            if (dV > minStep)
+            if (dV > deadzone)
             {
-                // Determine nearest "tap point", and winding ratio
-                var tap = Math.Round(20 * (self.Setting / inputVoltage));
-                var n = tap / 20;
+                // Determine nearest "tap point", and winding ratio (only set ratio in 1% increments)
+                var tap = Math.Round((self.Setting / inputVoltage) / step);
+                var n = tap * step;
 
-                n = Math.Clamp(n, 0.1, 10);
+                // Clamp 
+                n = Math.Clamp(n, nMin, nMax);
 
-                transformer.Deinitialize();
-                transformer.N = n;
-                transformer.Initialize();
+                if (n != transformer.N)
+                {
+                    transformer.Deinitialize();
+                    transformer.N = n;
+                    transformer.Initialize();
+                }
             }
         }
     }
